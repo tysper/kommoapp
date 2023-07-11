81bff9f9da5771f9c6da7d3c311d734a30427d91
Pedro Paulo <pedropaulo_nunes@outlook.com>
2023-07-11 18:33:41 -0300

    Parallel request added to visualize more info about the leads

--- kommoapp.py
+++ kommoapp.py
@@ -1,17 +1,25 @@
 import aiohttp
 import asyncio
 import json
+from datetime import datetime
+import time
 
-domain = input("Please input your domain: ")
+# domain = input("Please input your domain: ")
 
-async def main():
+domain = "keitex"
+base = f"https://{domain}.kommo.com"
+ids = []
+keys = []
+
+
+async def get_ids():
 
     secret_key = input("Please input your Secret Key: ")
     authorization_code = input("Please input your Authorization Code: ")
 
 
     async with aiohttp.ClientSession() as session: 
-        base = f"https://{domain}.kommo.com"
+        
 
         auth_url = base+"/oauth2/access_token"
         auth_body = {
@@ -35,7 +43,7 @@ async def main():
                 print("\n")
 
                 access_token = response_auth["access_token"]
-
+                
                 leads_url = base+"/api/v4/leads"
                 leads_headers = {
                     "Authorization": f"Bearer {access_token}"
@@ -44,11 +52,54 @@ async def main():
                     response_leads = await leads_response.json()
                     leads = response_leads["_embedded"]["leads"] #dict list
                     print(json.dumps(leads, indent=4))
+
+                    for lead in leads:
+                        ids.append(lead["id"])
+                
+                    keys.append(f"{access_token}")
             else:
                 print(response_auth)
                 print("\n\nPlease try again!\n")
-                await main()
+                await get_ids()
 
+#Get the ids from Kommmo
+asyncio.run(get_ids())
 
+async def fetch_info(session, id, token):
+    async with session.get(f"{base}/api/v4/leads/{id}", headers={"Authorization": f"Bearer {token}"}) as r:
+        if r.status != 200:
+            r.raise_for_status()
+        return await r.json()
 
-asyncio.run(main())
+async def fetch_all_info(session, ids, token):
+    tasks = []
+    for id in ids:
+        task = asyncio.create_task(fetch_info(session, id, token))
+        tasks.append(task)
+    res = await asyncio.gather(*tasks)
+    return res
+
+async def get_complete_info(limit):
+    async with aiohttp.ClientSession() as session:
+
+        for start in range(0, len(ids), limit):
+            ti = datetime.now().timestamp()
+            end = start+limit
+            if end>len(ids):
+                end=len(ids)
+
+            # print(f"Start: {start} End:{end}")
+
+            responses = await fetch_all_info(ids=ids[start:end], session=session, token=keys[0])
+            for response in responses:
+                print(response)
+
+            tf = datetime.now().timestamp()
+
+            # print(f"Time: {(tf-ti)}s")
+            #wait a second
+            while tf-ti < 1:
+                tf = datetime.now().timestamp()
+
+#Fetch the ids in parallel with a limit of 5 requests per second
+asyncio.run(get_complete_info(5))
